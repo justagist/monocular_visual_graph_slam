be8faad80e6dc40f6dcc564577e6dbc372748c56
Saif Sidhik <sxs1412@student.bham.ac.uk>
2017-07-13 11:22:14 +0100

    added datapool methods to get size of datapool and to obtain a dataspot from the pool

--- src/gslam/data_pool.cpp
+++ src/gslam/data_pool.cpp
@@ -20,9 +20,58 @@ void DataPool::addDataSpot(DataSpot3D::DataSpot3DPtr data_spot_ptr)
     fabmap_.compareAndAdd(data_spot_ptr, new_id, loop_id);
 
     std::cout << " LoopID: " << loop_id << std::endl;
+
     // if( loop_id >= 0 && new_id > 0 ){}  // TODO: loop closure constraint
 
+    std::cout << " Adding odometry " << std::endl;
+    // Odometry constraint
+    if( last_spot_.get() )
+    {
+        customtype::TransformSE3 rel_transform = last_spot_->getPose().inverse()*data_spot_ptr->getPose();
+        float cx, cy, fx, fy;
+        cx = last_spot_->getCamParams().cx_;
+        cy = last_spot_->getCamParams().cy_;
+        fx = last_spot_->getCamParams().fx_;
+        fy = last_spot_->getCamParams().fy_;
 
+        // std::cout << "ODOM: corr " <<  odom_correspondences << " info " << info << std::endl;
+
+        // DataLink3D::DataLinkPtr link( new DataLink3D() );
+
+        // // before was 100
+        // if( info > 0 && info < 1000000 ) link->inf_matrix_ *= info;
+        // else if (info > 1000000) link->inf_matrix_ *= 1000000;//0.5;
+        // else link->inf_matrix_ *= 10;//0.5;
+
+        // link->transform_ = rel_transform;
+        // link->from_id_ = last_spot_->getId();
+        // link->to_id_ = data_spot_ptr->getId();
+        // link->active = true;
+        // link->type = DataLink3D::OdomConstraint;
+
+        // last_spot_->addLink(link);
+
+        //link->inf_matrix_ // Identity
+
+
+    } // odometry constriant -- if()
+
+    data_spots_.insert(data_spots_.end(), std::make_pair(data_spot_ptr->getId(),data_spot_ptr));
+    last_spot_ = data_spot_ptr;
+}
+
+DataSpot3D::DataSpot3DPtr DataPool::getSpot(customtype::Identifier id){
+
+    DataSpot3D::DataSpotMap::iterator it = data_spots_.find(id);
+    if( it != data_spots_.end() )
+        return it->second;
+    else
+        return DataSpot3D::DataSpot3DPtr();
+
+}
+
+int DataPool::getSize(){
+    return data_spots_.size();
 }
 
 } // namespace gSlam
