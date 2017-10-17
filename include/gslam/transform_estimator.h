3ed11d9284857f5cc3c6af724af501f826df4a04
justagist <saifksidhik@gmail.com>
2017-10-17 22:11:03 +0530

    started removing dependencies on pcl

--- include/gslam/transform_estimator.h
+++ include/gslam/transform_estimator.h
@@ -10,11 +10,8 @@
 #ifndef __TRANSFORM_ESTIMATOR__
 #define __TRANSFORM_ESTIMATOR__
 
-// #include "typedefs.h"
 #include "gslam/data_spot.h"
 #include "gslam/image_matcher.h"
-#include "gslam/slam_utils.h"
-// #include "gslam/dataspot_matcher.h"
 
 namespace gSlam 
 {
@@ -26,17 +23,8 @@ public:
 
     TransformEstimator(){}
 
-    customtype::TransformSE3 estimateTransform(DataSpot3D::DataSpot3DPtr data_spot_src, DataSpot3D::DataSpot3DPtr data_spot_target, double& variance, int& correspondences, double & prop_match, bool & converge_status);
-
     customtype::TransformSE3 estimateTransformUsingOpticalFlow(DataSpot3D::DataSpot3DPtr data_spot_src, DataSpot3D::DataSpot3DPtr data_spot_target,
                                                                int& correspondences, int& max_correspondences, double& avg_error, bool& converge_status);
-
-private:
-    slam_utils::ImageMatcher spot_matcher_;
-    // float opt_flow_err_tol_;
-    // int opt_flow_min_match_reqd_;
-    // cv::Mat::intrinsics_, distortion_;
-    // int repeat_match_counter_, max_repeat_match_counter_;
 
 }; // class TransformEstimator
 
